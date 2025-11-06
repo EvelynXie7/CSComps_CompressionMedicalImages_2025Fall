4424de2c42514b18faebae2cdaf97fb9e39a9168
rui shen <ruishen@ruideMacBook-Pro-2.local>
2025-11-06 17:23:29 -0600

    dwt not revised for spiht

--- dwt.py
+++ dwt.py
@@ -1,13 +1,13 @@
 import pywt
 import numpy as np
 
-WAVELET = 'db1' # Chose Daubechiesfilter because that's what https://www.sciencedirect.com/science/article/pii/S2352914818302405 did
+WAVELET =  pywt.Wavelet('db1') # Chose Daubechiesfilter because that's what https://www.sciencedirect.com/science/article/pii/S2352914818302405 did
 # Should probably change from 'db1' after doing more research
 
 def combineDWTData(LL, LH, HL, HH):
-    L_data = np.hstack((LL, LH))
-    H_data = np.hstack((HL, HH))
-    return np.vstack((L_data, H_data))
+    L_data = np.vstack((LL, LH))
+    H_data = np.vstack((HL, HH))
+    return np.hstack((L_data, H_data))
 
 
 def separateDWTData(dwt_data):
