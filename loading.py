0c331b814cf2e82d5a3b32754231cb9ea8a925e0
Lucie Wolf <lucie@LWMacbook.local>
2025-11-19 11:53:36 -0600

    Removed JPEG metrics files

--- loading.py
+++ loading.py
@@ -13,7 +13,6 @@ import utils
 def loadCT(case_num):
     case_path = get_case_path(case_num)
     vol = nib.load(str(case_path / "imaging.nii.gz"))
-    seg = nib.load(str(case_path / "segmentation.nii.gz"))
     
     vol_data = vol.get_fdata()
     vol_data = hu_to_grayscale(vol_data, DEFAULT_HU_MIN, DEFAULT_HU_MAX)[:, :, :, 0]
